7c037736cbdfede3b2c0690321b999b030bbbba9
HeshamOsman <hesham.osman28@yahoo.com>
2020-02-16 01:13:52 +0200

    Initialize project structure
    Add database schema and initialize project structure

---
+++ src/main/java/com/api/sercive/embltask/EmblTaskApplication.java
@@ -0,0 +1,13 @@
+package com.api.sercive.embltask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmblTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmblTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/sercive/embltask/EmblTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.sercive.embltask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmblTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
